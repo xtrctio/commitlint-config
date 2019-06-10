9434f368f9ce60d816ad3560061a65dd6d0ea105
Eric Hacke <ehacke@gmail.com>
2019-06-09 21:21:15 -0400

    feat: first

---
+++ index.js
@@ -0,0 +1,8 @@
+'use strict';
+
+module.exports = {
+  rules: {
+    'type-enum': [2, 'always', ['build', 'ci', 'chore', 'docs', 'feat', 'fix', 'merge', 'perf', 'refactor', 'revert', 'style', 'test', 'wip']],
+  },
+  extends: ['@commitlint/config-conventional'],
+};
